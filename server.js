78ee21cdf4057407c27c97d97c6de162ec7ae24c
Sharni Zaugg <sharni.greener@gmail.com>
2021-05-17 20:31:20 -0600

    playing with ports

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 })
-app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(3001, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
