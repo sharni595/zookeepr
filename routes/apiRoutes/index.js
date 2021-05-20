d5f07e3f84bd2bccc364d4c8fcc6e59a48b287d2
Sharni Zaugg <sharni.greener@gmail.com>
2021-05-20 16:29:14 -0600

    add zookeeper info

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
+
+
+
+
+router.use(zookeeperRoutes);
+router.use(animalRoutes);
+
+
+
+module.exports = router;
